1a2e58f0767a440048d2ec0e30d9d58f8a521e29
hallowshaw <rhitam.rit54@gmail.com>
2024-05-08 02:33:22 +0530

    Added CrpytoConext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,58 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { ThemeProvider, styled } from "@mui/material/styles";
+import createTheme from "@mui/material/styles/createTheme";
+import { useNavigate } from "react-router-dom";
+
+const HeaderContainer = styled("div")(() => ({
+  flex: 1,
+  color: "gold",
+  fontFamily: "Montserrat",
+  fontWeight: "bold",
+  cursor: "pointer",
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function Header() {
+  const navigateTo = useNavigate(); // Corrected the variable name to history
   return (
-    <div>
-      <AppBar color="primary" position="static"></AppBar>
-      <Container>
-        <Toolbar>
-          <Typography>DigitalDimeVault</Typography>
-        </Toolbar>
-      </Container>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <HeaderContainer>
+              <Typography onClick={() => navigateTo("/")} variant="h6">
+                DigitalDimeVault
+              </Typography>
+            </HeaderContainer>
+
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 }
 
