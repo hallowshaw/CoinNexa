09bf24f66d41cb620e98bd54449463af04ac7f12
hallowshaw <rhitam.rit54@gmail.com>
2024-05-11 14:58:45 +0530

    Added dangerouslySetInnerHTML to parse react-html

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { ThemeProvider, styled } from "@mui/material/styles";
 import createTheme from "@mui/material/styles/createTheme";
 import CoinInfo from "./../components/CoinInfo";
+import { Typography } from "@mui/material";
 
 function CoinPage() {
   const { id } = useParams();
@@ -23,17 +24,68 @@ function CoinPage() {
     fetchCoins();
   }, []);
 
-  const container = styled("div")(() => ({}));
-  const sidebar = styled("div")(() => ({}));
-  const container1 = styled("div")(() => ({}));
+  const MainContainer = styled("div")(() => ({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "@media (min-width: 960px)": {
+      flexDirection: "row",
+      alignItems: "unset",
+    },
+  }));
+  const Sidebar = styled("div")(() => ({
+    width: "30%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 25,
+    borderRight: "2px solid grey",
+    "@media (max-width: 960px)": {
+      width: "100%",
+      borderRight: "none",
+    },
+  }));
+
+  const Heading = styled("div")(() => ({
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontFamily: "Montserrat",
+  }));
+
+  const Description = styled("div")(() => ({
+    width: "100%",
+    fontFamily: "Montserrat",
+    padding: 25,
+    paddingBottom: 15,
+    paddingTop: 0,
+    textAlign: "justify",
+  }));
 
   return (
-    <div container>
-      {/* Sidebar */}
-      <div sidebar></div>
-      {/* Chart */}
-      <CoinInfo coin={coin} />
-    </div>
+    <>
+      <MainContainer>
+        <Sidebar>
+          <img
+            src={coin?.image.large}
+            alt={coin?.name}
+            height="200"
+            style={{ marginBottom: 20 }}
+          />
+          <Typography variant="h3">
+            <Heading>{coin?.name}</Heading>
+          </Typography>
+          <Typography variant="subtitle1">
+            <Description
+              dangerouslySetInnerHTML={{
+                __html: coin?.description?.en.split(". ")[0],
+              }}
+            />
+          </Typography>
+        </Sidebar>
+        {/* Chart */}
+        <CoinInfo coin={coin} />
+      </MainContainer>
+    </>
   );
 }
 
