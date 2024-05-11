825ba85baf2a0c9083941131022a667f7ee4efd9
hallowshaw <rhitam.rit54@gmail.com>
2024-05-11 14:00:12 +0530

    Created Coin Info Page

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+import { SingleCoin } from "../config/api";
+import axios from "axios";
+import { ThemeProvider, styled } from "@mui/material/styles";
+import createTheme from "@mui/material/styles/createTheme";
+import CoinInfo from "./../components/CoinInfo";
 
 function CoinPage() {
-  return <div>CoinPage</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoins = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  const container = styled("div")(() => ({}));
+  const sidebar = styled("div")(() => ({}));
+  const container1 = styled("div")(() => ({}));
+
+  return (
+    <div container>
+      {/* Sidebar */}
+      <div sidebar></div>
+      {/* Chart */}
+      <CoinInfo coin={coin} />
+    </div>
+  );
 }
 
 export default CoinPage;
